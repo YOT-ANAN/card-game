70dd476dfe7df8e9b0167533938a897b8f0b40a6
YOT-ANAN <5910110270@psu.ac.th>
2018-09-17 11:45:31 +0700

    commit More Components&Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard'
 
+const word = "Hello";
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+      <div>{
+        Array.from(word).map((c, i) => 
+        < CharacterCard value={c} key={i} />)
+        }
       </div>
     );
   }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 export default class CharacterCard extends Component{
     render(){
         return (
