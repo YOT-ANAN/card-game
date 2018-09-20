cf725b5516e0eae22d3a61805d6d3fc439242faa
YOT-ANAN <5910110270@psu.ac.th>
2018-09-20 11:39:48 +0700

    commit Components & States // fix error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div>
       {
-        <WordCard value="hello"/>
+        <WordCard value="HELLO"/>
       }
       </div>
     );
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class CharacterCard extends Component{
-    
     constructor(props){
-    
         super(props)
-        this.state={
-          active: false,
-        }
-        activate = () =>{
-          if(!this.state.active){
-              this.setState({active: true})
-          }
-        }
-    
-      }
-    render(){
-        let className = `card ${this.state.active ? 'activeCard': ''}`
-
-        return (
-            <div  className={className} onClick={this.activate} >
-            {
-                this.props.value
-            }</div>
-        )
+        this.state = {
+            active: false,
+             }
     }
+       
+        activate = () => {
+            if(!this.state.active){
+            this.setState({active: true})
+             }
+        }
+        render(){
+            let className = `card ${this.state.active ? 'activeCard': ''}`
+            return (
+                <div className={className} onClick={this.activate}>
+                    {this.props.value}
+                </div>
+            )
+        }
 }
+       
