7f9e5aee10771151afbb504c08f25d462bee57cd
YOT-ANAN <5910110270@psu.ac.th>
2018-09-21 22:26:54 +0700

    commit GameLogic

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 export default class CharacterCard extends Component{
     constructor(props){
         super(props)
@@ -8,20 +9,24 @@ export default class CharacterCard extends Component{
             active: false,
              }
     }
-       
-        activate = () => {
-            if(!this.state.active){
+    componentDidUpdate(prevProps){
+        if(prevProps.attempt != this.props.attempt){
+            this.setState({active: false})
+        }
+    }    
+    activate = () => {
+        if(!this.state.active){
             this.props.activationHandler(this.props.value)
             this.setState({active: true})
-             }
         }
-        render(){
-            let className = `card ${this.state.active ? 'activeCard': ''}`
+    }
+    render(){
+        let className = `card ${this.state.active ? 'activeCard': ''}`
             return (
                 <div className={className} onClick={this.activate}>
                     {this.props.value}
                 </div>
             )
-        }
+    }
 }
        
